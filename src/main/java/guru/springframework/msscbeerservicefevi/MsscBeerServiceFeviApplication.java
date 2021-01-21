bf27c2612ed5eabad332251d685ad440694e518b
federicoviscomi <federicoviscomi@gmail.com>
2021-01-21 11:54:12 +0000

    init

---
+++ src/main/java/guru/springframework/msscbeerservicefevi/MsscBeerServiceFeviApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservicefevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceFeviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceFeviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/msscbeerservicefevi/MsscBeerServiceFeviApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservicefevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceFeviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
